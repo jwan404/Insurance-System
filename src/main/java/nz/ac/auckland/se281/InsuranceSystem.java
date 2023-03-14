0e845de79889a5a3ca018fc8da61760807d7a752
jwan404 <jwan404@aucklanduni.ac.nz>
2023-03-15 12:56:32 +1300

    class for profile

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,33 +1,45 @@
 package nz.ac.auckland.se281;
-
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
+  private int nProfile;
+
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
-   
   }
 
   public void printDatabase() {
-    System.out.println("Database has 0 profiles.");
-    //make a reference point 
-    
+    //System.out.println("Database has 1 profile: ");
+    if (nProfile == 0) {
+      Profile p0 = new Profile();
+      p0.zeroProfiles();
+    }
+    else if (nProfile == 1) {
+      Profile p1 = new Profile();
+      p1.oneProfile();
+    }
+    else {
+      Profile p2 = new Profile();
+      p2.twoProfiles();
+    }
   }
 
   public void createNewProfile(String userName, String age) {
+    
+    //Profile p1 = new Profile();
     int size = userName.length();
     int nProfile = 0;
 
     if (size >= 3) {
       nProfile++;
        if (nProfile == 1) {
-        System.out.println("Database has 1 profile: ");
+        printDatabase();//System.out.println("Database has 1 profile: "); //ref
         System.out.println("New profile created for " + userName + " " + "with age " + age + ".");
         System.out.println(" " + "1" + ":" + " " + userName + ", " + age);
       }
       else {
+        printDatabase(); //System.out.println("Database has 2 profiles: ");//ref
         int rank = 1;
-        System.out.println("Database has 2 profiles: ");
         System.out.println(" " + rank + ":" + " " + userName + ", " + age);
         rank++;
         System.out.println(" " + rank + ":" + " " + userName + ", " + age);
--- src/main/java/nz/ac/auckland/se281/Profile.java
+++ src/main/java/nz/ac/auckland/se281/Profile.java
@@ -1,8 +1,26 @@
+package nz.ac.auckland.se281;
+
 public class Profile {
-    private String userName;
-    private int age;
+   /* private String userName;
+    private String age;
+    private String rank;
+*/ 
+
+    /*public Profile (String userName, String age) {
+        this.userName = userName;
+        this.age = age;*/
+
+
+    public void zeroProfiles(){
+        MessageCli.PRINT_DB_POLICY_COUNT.getMessage("0", "s", "."); 
+    }
+
+    public void oneProfile(){
+        MessageCli.PRINT_DB_POLICY_COUNT.getMessage("1", "", ":"); 
+    }
 
     public void twoProfiles(){
         MessageCli.PRINT_DB_POLICY_COUNT.getMessage("2", "s", ":"); 
+
     }
 }
