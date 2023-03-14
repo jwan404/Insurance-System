ae3dccbf204cd956a46925f843f3918d404f1dc0
jwan404 <jwan404@aucklanduni.ac.nz>
2023-03-15 10:53:40 +1300

    create profile

---
+++ src/main/java/nz/ac/auckland/se281/Profile.java
@@ -0,0 +1,8 @@
+public class Profile {
+    private String userName;
+    private int age;
+
+    public void twoProfiles(){
+        MessageCli.PRINT_DB_POLICY_COUNT.getMessage("2", "s", ":"); 
+    }
+}
