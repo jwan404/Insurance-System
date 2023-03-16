1a4da4216ea8f423f860e36ffe322fcc175dcfa4
jwan404 <jwan404@aucklanduni.ac.nz>
2023-03-16 18:47:54 +1300

    ready for gradestyle

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -11,6 +11,7 @@ public class InsuranceSystem {
   }
 
   public void printDatabase() {
+    // When database is called, checks database for number of profiles and outputs messages depending on the number of profiles.
     if (db.size() == 0) {
       System.out.println(MessageCli.PRINT_DB_POLICY_COUNT.getMessage("0", "s", "."));
       return;
@@ -33,27 +34,28 @@ public class InsuranceSystem {
   }
 
   public void createNewProfile(String userName, String age) {
+    // Creates the profile while ignoring the case. And checks whether the name is valid.
     userName = userName.substring(0,1).toUpperCase() + userName.substring(1, userName.length()).toLowerCase();
-    int numAge = Integer.parseInt(age);
+    int numAge = Integer.parseInt(age); // new variable that converts string age to int age
     
-    if (userName.length() < 3) {
+    if (userName.length() < 3) { // Checks if name is long enough
         System.out.println(MessageCli.INVALID_USERNAME_TOO_SHORT.getMessage(userName));
         return;
     }
     
-     if (numAge < 0 ) {
+     if (numAge < 0 ) { // Checks if age is a positive integer
       System.out.println(MessageCli.INVALID_AGE.getMessage(age, userName));
       return;
     }
 
-    for (int i = 0; i < db.size(); i++) {
+    for (int i = 0; i < db.size(); i++) { //checks if name is unique
       if(db.get(i).getName() == userName){
         System.out.println(MessageCli.INVALID_USERNAME_NOT_UNIQUE);
         return;
       }
     }
 
-      db.add(new Profile(userName, age));
+      db.add(new Profile(userName, age)); //adds profile to database
       System.out.println("New profile created for " + userName + " " + "with age " + age + ".");
 
   }
