d557173984b90a788f54509cda385f08949cdf3b
jwan404 <jwan404@aucklanduni.ac.nz>
2023-03-16 18:35:05 +1300

    ready for gradestyle

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -16,42 +16,46 @@ public class InsuranceSystem {
       return;
     }
 
+    if (db.size() == 1) {
+      System.out.println( MessageCli.PRINT_DB_POLICY_COUNT.getMessage("1", "", ":"));
+      System.out.println(" " + (1) + ":" + " " + db.get(1).getName() + ", " + db.get(1).getAge());  
+      return;  
+    }
+
+    System.out.println( MessageCli.PRINT_DB_POLICY_COUNT.getMessage(String.format("%d", db.size()), "s", ":"));
+
     for (int i = 0; i < db.size(); i++) {
-      if (db.size() == 1) {
-        System.out.println( MessageCli.PRINT_DB_POLICY_COUNT.getMessage("1", "", ":"));
-        System.out.println(" " + (i+1) + ":" + " " + db.get(i).getName() + ", " + db.get(i).getAge());    
-      }
-      else {
-        System.out.println( MessageCli.PRINT_DB_POLICY_COUNT.getMessage(String.format("%d", i), "s", ":"));
-        System.out.println(" " + (i+1) + ":" + " " + db.get(i).getName() + ", " + db.get(i).getAge());    
-      }
+      
+     
+      System.out.println(" " + (i+1) + ":" + " " + db.get(i).getName() + ", " + db.get(i).getAge());    
+
     }
   }
 
   public void createNewProfile(String userName, String age) {
+    userName = userName.substring(0,1).toUpperCase() + userName.substring(1, userName.length()).toLowerCase();
     int numAge = Integer.parseInt(age);
     
     if (userName.length() < 3) {
-        MessageCli.INVALID_USERNAME_TOO_SHORT.getMessage(userName);
+        System.out.println(MessageCli.INVALID_USERNAME_TOO_SHORT.getMessage(userName));
         return;
     }
     
      if (numAge < 0 ) {
-      MessageCli.INVALID_AGE.getMessage(age, userName);
+      System.out.println(MessageCli.INVALID_AGE.getMessage(age, userName));
       return;
     }
 
     for (int i = 0; i < db.size(); i++) {
       if(db.get(i).getName() == userName){
-        // Message
+        System.out.println(MessageCli.INVALID_USERNAME_NOT_UNIQUE);
         return;
       }
     }
 
-    db.add(new Profile(userName, age));
-    System.out.println("New profile created for " + userName + " " + "with age " + age + ".");
-    
-  
+      db.add(new Profile(userName, age));
+      System.out.println("New profile created for " + userName + " " + "with age " + age + ".");
+
   }
       
 
